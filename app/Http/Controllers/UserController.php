cf20768d46d3b50b6a266117ba7ba10fb76db437
code1minh <code1minh@gmail.com>
2022-11-20 21:44:44 +0700

    #22

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -52,6 +52,27 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        return $request;
+        $validated = $request->validate([
+            "status_id" => "required",
+            "username" => "required|unique:users,username",
+            "name" => "required|max:255",
+            "email" => "required|email",
+            "department_id" => "required",
+            "password" => "required|confirmed"
+        ], [
+            "status_id.required" => "Nhập Tình trạng",
+            "username.required" => "Nhập Tên Tài khoản",
+            "username.unique" => "Tên Tài khoản đã tồn tại",
+
+            "name.required" => "Nhập Họ và Tên",
+            "name.max" => "Ký tự tối đa là 255",
+
+            "email.required" => "Nhập Email",
+            "email.email" => "Email không hợp lệ",
+
+            "department_id.required" => "Nhập Phòng ban",
+            "password.required" => "Nhập Mật khẩu",
+            "password.confirmed" => "Mật khẩu và Xác nhận mật khẩu không khớp"
+        ]);
     }
 }
