717dc6fe9b8100fba674f9f11746161dba6e785f
code1minh <code1minh@gmail.com>
2022-11-21 20:47:39 +0700

    #23

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,14 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    //     'username'
+    // ];
+
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
